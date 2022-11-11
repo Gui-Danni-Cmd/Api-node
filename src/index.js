4e7bfe913fbef6c5f4469029701e42291cc2b9bf
Gui-Danni-Cmd <66735899+Gui-Danni-Cmd@users.noreply.github.com>
2022-11-11 18:38:27 -0300

    express conecte

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
-const body_parse = require('body-parse')
+const body_parse = require('body-parser')
 
 const app = express()
 
 app.use(body_parse.json())
 app.use(body_parse.urlencoded({ extended: false }))
 
+app.get('/', (req, res) => {
+    res.send('Conectado')
+})
 
 app.listen(3000)
